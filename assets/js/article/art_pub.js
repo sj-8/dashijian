49ab5370ff6fda269f77d36aa12065f4f2c04aec
sj <872525592@qq.com>
2020-09-09 15:51:58 +0800

    完成除了发布以外的功能

---
+++ assets/js/article/art_pub.js
@@ -0,0 +1,46 @@
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+
+    initCate()
+
+    function initCate() {
+        $.ajax({
+            type: "GET",
+            url: "/my/article/cates",
+
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('初始化文章分类失败！')
+
+                }
+                // 调用模板引擎，渲染分类的下拉菜单
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                // 一定要记得调用 form.render() 方法
+                form.render()
+            }
+        });
+    }
+
+    // 初始化富文本编辑器
+    initEditor()
+
+
+    // 1. 初始化图片裁剪器
+    var $image = $('#image')
+
+    // 2. 裁剪选项
+    var options = {
+        aspectRatio: 400 / 280,
+        preview: '.img-preview'
+    }
+
+    // 3. 初始化裁剪区域
+    $image.cropper(options)
+
+    // 为选择封面的按钮，绑定点击事件处理函数
+    $('#btnChooseImage').on('click', function () {
+        $('#coverFile').click()
+    })
+})
